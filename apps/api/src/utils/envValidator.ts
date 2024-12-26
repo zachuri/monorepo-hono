5752867451c356348d62d62270b6dbf03584e2cd
Turbobot <turbobot@vercel.com>
2024-12-26 13:56:04 -0800

    feat(create-turbo): apply official-starter transform

--- apps/api/src/utils/envValidator.ts
+++ apps/api/src/utils/envValidator.ts
@@ -2,19 +2,20 @@ import { config } from 'dotenv';
 import type { TypeOf } from 'zod';
 import { z } from 'zod';
 
+import { EnvSchema } from '@/types/env';
+
 config({
   path: '.dev.vars',
 });
 
-const zodEnv = z.object({
-  DATABASE_URL: z.string(),
-});
+const zodEnv = EnvSchema;
 
 declare global {
   namespace NodeJS {
     interface ProcessEnv extends TypeOf<typeof zodEnv> {}
   }
 }
+
 try {
   zodEnv.parse(process.env);
 } catch (err) {
