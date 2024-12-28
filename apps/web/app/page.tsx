56716e8e308fd14493f2e0717bdb035720734adc
zachuri <punsalang.zachary@gmail.com>
2024-12-28 00:33:13 -0800

    chore: updated ui need to fix web

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,7 +1,7 @@
 import Image, { type ImageProps } from "next/image";
-import { Button } from "@repo/ui/button";
 import styles from "./page.module.css";
-import { AppType } from '@repo/api/src';
+import { AppType } from "@repo/api/src";
+import { Button } from '@repo/ui/components/ui/button';
 
 type Props = Omit<ImageProps, "src"> & {
 	srcLight: string;
