f0a0b15547675c3bd14d74dc4c35366ef390973a
zac <punsalang.zachary@gmail.com>
2025-02-08 03:35:00 -0800

    auth: discord add [APT-14] (#11)
    * fix: added server env within next for schema validation parsing
    
    * feat: added discord auth

--- packages/app/env/api.ts
+++ packages/app/env/api.ts
@@ -7,6 +7,8 @@ const EnvSchema = z.object({
   GITHUB_CLIENT_SECRET: z.string(),
   GOOGLE_CLIENT_ID: z.string(),
   GOOGLE_CLIENT_SECRET: z.string(),
+  DISCORD_CLIENT_ID: z.string(),
+  DISCORD_CLIENT_SECRET: z.string(),
   APPLE_CLIENT_ID: z.string(),
   APPLE_PRIVATE_KEY: z.string(),
   APPLE_TEAM_ID: z.string(),
