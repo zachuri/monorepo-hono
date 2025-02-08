f739b856b69c1df5bb49b10128c314fdc3adb4c7
zachuri <punsalang.zachary@gmail.com>
2025-02-08 02:39:16 -0800

    chore: feat setup biome settings similar to airbnb

--- packages/api/src/lib/auth.ts
+++ packages/api/src/lib/auth.ts
@@ -6,38 +6,38 @@ import { env } from 'hono/adapter'
 
 // Load environment variables
 const createBetterAuthConfig = (dbInstance: any, c: Context<AppContext>) => ({
-	baseURL: env(c).API_DOMAIN, // API URL
-	trustedOrigins: [env(c).API_DOMAIN, env(c).WEB_DOMAIN], // Needed for cross domain cookies
-	database: drizzleAdapter(dbInstance, {
-		provider: "pg", // or "mysql", "sqlite"
-	}),
-	emailAndPassword: {
-		enabled: true,
-	},
-	socialProviders: {
-		github: {
-			clientId: env(c).GITHUB_CLIENT_ID,
-			clientSecret: env(c).GITHUB_CLIENT_SECRET,
-		},
-		google: {
-			clientId: env(c).GOOGLE_CLIENT_ID,
-			clientSecret: env(c).GOOGLE_CLIENT_SECRET,
-		},
-	},
-	advanced: {
-		crossSubDomainCookies: {
-			enabled: true, // Enables cross-domain cookies
-		},
-		defaultCookieAttributes: {
-			sameSite: "none", // Required for cross-domain cookies
-			secure: true, // Ensures cookies are only sent over HTTPS
-		},
-	},
-	rateLimit: {
-		window: 10, // time window in seconds
-		max: 100, // max requests in the window
-	},
-});
+  baseURL: env(c).API_DOMAIN, // API URL
+  trustedOrigins: [env(c).API_DOMAIN, env(c).WEB_DOMAIN], // Needed for cross domain cookies
+  database: drizzleAdapter(dbInstance, {
+    provider: 'pg', // or "mysql", "sqlite"
+  }),
+  emailAndPassword: {
+    enabled: true,
+  },
+  socialProviders: {
+    github: {
+      clientId: env(c).GITHUB_CLIENT_ID,
+      clientSecret: env(c).GITHUB_CLIENT_SECRET,
+    },
+    google: {
+      clientId: env(c).GOOGLE_CLIENT_ID,
+      clientSecret: env(c).GOOGLE_CLIENT_SECRET,
+    },
+  },
+  advanced: {
+    crossSubDomainCookies: {
+      enabled: true, // Enables cross-domain cookies
+    },
+    defaultCookieAttributes: {
+      sameSite: 'none', // Required for cross-domain cookies
+      secure: true, // Ensures cookies are only sent over HTTPS
+    },
+  },
+  rateLimit: {
+    window: 10, // time window in seconds
+    max: 100, // max requests in the window
+  },
+})
 
 export const initializeBetterAuth = (c: Context<AppContext>) => {
   const db = c.get('db')
