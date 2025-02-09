12c8940943de0738c006bc41fdd9f7998e7245eb
zachuri <punsalang.zachary@gmail.com>
2025-02-08 22:37:30 -0800

    chore: removed hello route

--- packages/api/src/routes/hello/hello.route.ts
+++
@@ -1,7 +0,0 @@
-import { Hono } from 'hono'
-
-import type { AppContext } from '~/types/app-context'
-
-export const helloRouter = new Hono<AppContext>().get('/', (c) => {
-  return c.json({ message: 'hello' })
-})
