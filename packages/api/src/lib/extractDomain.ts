965d35b96ffcb61431a998d10294f08eed758ca7
zac <punsalang.zachary@gmail.com>
2025-02-22 20:45:10 -0800

    chore(domain): extract domain updated with all suffix types

--- packages/api/src/lib/extractDomain.ts
+++ packages/api/src/lib/extractDomain.ts
@@ -1,14 +1,11 @@
+import { parse } from 'tldts'
+
 export function extractDomain(url: string): string {
   try {
-    const hostname = new URL(url).hostname;
-    const domainParts = hostname.split('.');
-    // Assuming you want the last two parts of the domain (e.g., zachuri.com)
-    if (domainParts.length > 1) {
-      return `.${domainParts.slice(-2).join('.')}`;
-    }
-    return hostname;
+    const domain = parse(url).domain
+    return domain ?? ''
   } catch (error) {
-    console.error('Invalid URL:', error);
-    return '';
+    console.error('Invalid URL:', error)
+    return ''
   }
 }
