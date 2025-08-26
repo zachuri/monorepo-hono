71c65e6871a080e85c8c5f0ddf3dac8aa49f17b7
zac <punsalang.zachary@gmail.com>
2025-08-25 17:08:34 -0700

    setup: use bun (#20)
    * setup: use bun
    
    * fix: configure open api: target key with js

--- packages/api/src/lib/configure-open-api.ts
+++ packages/api/src/lib/configure-open-api.ts
@@ -18,7 +18,7 @@ export default function configureOpenAPI(app: AppOpenAPI) {
       theme: 'kepler',
       layout: 'classic',
       defaultHttpClient: {
-        targetKey: 'javascript',
+        targetKey: 'js',
         clientKey: 'fetch',
       },
       spec: {
