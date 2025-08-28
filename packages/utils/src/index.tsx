caef1d2c1f8ab1eccc40c350b2dce400799d7cf9
zac <punsalang.zachary@gmail.com>
2025-08-28 01:30:10 -0700

    chore: removed utils

--- packages/utils/src/index.tsx
+++
@@ -1,7 +0,0 @@
-export * from './supabase/client';
-export * from './supabase/middleware';
-export * from './supabase/server';
-export * from './supabase/useUser';
-export { toSlug } from './toSlug';
-export { useIsomorphicLayoutEffect } from './useIsomorphicLayoutEffect';
-export { usePrevious } from './usePrevious';
